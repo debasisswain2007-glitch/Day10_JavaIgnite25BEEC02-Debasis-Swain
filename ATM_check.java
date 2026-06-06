c553236659f7871953e25e89897a429ef5ef0d67
debasisswain2007-glitch <debasis.swain2007@gmail.com>
2026-06-06 08:14:44 +0530

    Update ATM_check.java25BEEC02_Debasis Swain

--- ATM_check.java
+++ ATM_check.java
@@ -11,3 +11,35 @@ If input is invalid (text instead of number) → handle exception
 Expected Output:
 Transaction Successful / Insufficient Balance / Invalid Input
 */
+import java.util.Scanner;
+
+public class ATM {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        try {
+            System.out.print("Enter balance: ");
+            double balance = Double.parseDouble(sc.nextLine());
+            
+            System.out.print("Enter withdrawal amount: ");
+            double withdrawal = Double.parseDouble(sc.nextLine());
+            
+            if (withdrawal < 0) 
+                throw new IllegalArgumentException("Negative withdrawal amount");
+            
+            if (withdrawal > balance) 
+                throw new ArithmeticException("Insufficient Balance");
+            
+            System.out.println("Transaction Successful");
+            
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid Input");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Invalid Input");
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        }
+        
+        sc.close();
+    }
+}
